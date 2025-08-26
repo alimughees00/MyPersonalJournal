c55def15149eb8bbeef7453154bcf1f26cec6747
shoaib836 <muhammadshoaib8274@gmail.com>
2025-08-26 19:17:53 +0500

    Version change

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -21,7 +21,7 @@ import {
   heightPercentageToDP as hp,
 } from 'react-native-responsive-screen';
 
-const BUILD_NUMBER = '1.0.6';
+const BUILD_NUMBER = '1.0.7';
 const FEEDBACK_EMAIL = 'feedback@baltorotech.com';
 // ... (other imports remain the same)
 
